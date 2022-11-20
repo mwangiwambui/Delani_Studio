e2e72663d891939ec93684bdeafeb0ee22060071
Naomi Wambui Mwangi <nwmwangi@safaricom.co.ke>
2022-11-20 13:46:33 +0300

    Finished the work we do section

--- js/script.js
+++ js/script.js
@@ -5,13 +5,31 @@ $(document).ready(function(){
         $("#overlay").hide();
       });
 
-    $("#wid-image").click(function(){
-        $("#wid-image").slideDown('1500').hide('1000');
-        $("#wid").show('1500');
+    $("#design-image").click(function(){
+        $("#design-image").slideDown('1500').hide('1000');
+        $("#design").show('1500');
       });
-      $("#wid").click(function(){
-        $("#wid").slideUp('1500');
-        $("#wid-image").slideDown('1500');
+      $("#design").click(function(){
+        $("#design").slideUp('1500');
+        $("#design-image").slideDown('1500');
+      });
+
+      $("#development-image").click(function(){
+        $("#development-image").slideDown('1500').hide('1000');
+        $("#development").show('1500');
+      });
+      $("#development").click(function(){
+        $("#development").slideUp('1500');
+        $("#development-image").slideDown('1500');
+      });
+
+      $("#product-image").click(function(){
+        $("#product-image").slideDown('1500').hide('1000');
+        $("#product").show('1500');
+      });
+      $("#product").click(function(){
+        $("#product").slideUp('1500');
+        $("#product-image").slideDown('1500');
       });
 
       $("form#form-contact").submit(function(event){
